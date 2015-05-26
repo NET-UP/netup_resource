99dda7e599c230a7c15955848ffe8fd852913c67
zitterf <fabian.zitter@net-up.de>
2015-05-26 17:20:10 +0200

    NetupResource::Base moved to core file

---
+++ lib/netup_resource/core.rb
@@ -0,0 +1,95 @@
+module NetupResource  
+  class Base
+
+    class << self
+      #base url attribute; type: string
+      attr_accessor :url
+      # format type
+      attr_accessor :type
+      attr_accessor :request_type
+      attr_accessor :response_type
+      #optional response object schema
+      attr_accessor :schema
+      #optional ssl connector
+      attr_accessor :ssl
+      attr_accessor :accessing_user_id
+      #GET-Request
+      # uri: api-uri
+      # parameters: request params hash(optional)
+      # auth: base aut(optional)
+      def get(uri="",parameters={},auth=nil)
+        return request(@url+uri,parameters,auth,:get)
+      end
+      #POST-Request
+      # uri: api-uri
+      # parameters: request params hash(optional)
+      # auth: base aut(optional)
+      def post(uri="",parameters={},auth=nil)
+        return request(@url+uri,parameters,auth,:post)
+      end
+
+      #PUT-Request
+      # uri: api-uri
+      # parameters: request params hash(optional)
+      # auth: base aut(optional)
+      def put(uri="",parameters={},auth=nil)
+        return request(@url+uri,parameters,auth,:put)
+      end
+
+      #DELETE-Request
+      # uri: api-uri
+      # parameters: request params hash(optional)
+      # auth: base aut(optional)
+      def delete(uri="",parameters={},auth=nil)
+        return request(@url+uri,parameters,auth,:delete)
+      end
+      #URL-Setter
+      def url=(u)
+        @url = u
+      end
+      #Type-Setter
+      def type=(t)
+        @type = t
+      end
+      #Schema-Setter
+      def schema=(s)
+        @schema = s
+      end
+      #ssl-Setter
+      def ssl=(ssl)
+        @ssl = ssl
+      end
+      private
+      #general request function
+      def request(uri="",parameters={},auth=nil,type)
+        ### DAS HIER ($accessing_user_id) IST EINE GLOBALE VARIABLE
+        ### SIE WIRD DER API ÜBERGEBEN UND ENTHÄLT DEN EINGELOGGTEN USER (FALLS VORHANDEN)
+        parameters[:accessing_user_id] ||= $accessing_user_id  #self.accessing_user_id
+        ### NIEMALS FÜR IRGENDETWAS ANDERES VERWENDEN!!!
+        ### AM BESTEN EINFACH FINGER WEG!!!
+        answer = NetupResource::HttP.call(uri,parameters,auth,@ssl,type,formats)
+        resp_obj = NetupResource::ResponseObject.create(@schema)
+        response = resp_obj.new
+        if @schema
+          for i in (0...@schema.length)
+            response.instance_variable_set("@#{@schema[i].to_s}".to_sym,answer[@schema[i].to_s])
+          end
+        elsif YamL.schema_exists?(self.name.downcase)
+          path = "#{Rails.root}/config/netup_resource/schema/#{self.name.downcase}.yml"
+          return YamL.data_object(path,answer)
+        else
+          return ResponseObject.auto_detect(answer)
+        end
+        return response
+      end
+
+      def formats
+        {
+          :type => type,
+          :request_type => request_type,
+          :response_type => response_type
+        }
+      end
+    end
+  end
+end
