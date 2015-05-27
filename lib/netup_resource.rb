29623918dcf4b0d4c4b4672b13c7d4b63c4a3b08
zitterf <fabian.zitter@net-up.de>
2015-05-27 09:34:02 +0200

    doc

--- lib/netup_resource.rb
+++ lib/netup_resource.rb
@@ -72,58 +72,12 @@
 # )
 #
 #
+
 module NetupResource
   require "netup_resource/yaml"
   require "netup_resource/core"
   require "netup_resource/http"
   require "netup_resource/response_object"
-end
-
-class Array
-  def is_a_record?
-    if self.length > 0
-      answer = true
-      self.each {|i| answer = false if !i.is_a?(Hash)}
-      return false unless answer
-      schema = self[0].keys
-      self.each {|i| answer = false if !i.keys == schema}
-      return answer
-    else
-      return false
-    end
-  end
-end
-
-class Hash
-  def flatten_keys(newhash={}, keys=nil)
-    self.each do |k, v|
-      k = k.to_s
-      keys2 = keys ? keys+"[#{k}]" : k
-      if v.is_a?(Hash)
-        v.flatten_keys(newhash, keys2)
-      else
-        newhash[keys2] = v
-      end
-    end
-    newhash
-  end
-
-  def format_form_data
-    result = {}
-    self.each do |k,v|
-      if v.is_a? Hash
-         v = v.flatten_keys
-         result[k.to_s] = format_form_data(v)
-      elsif v.is_a? Array
-         v.length.times do |n|
-           v[n] = v[n].flatten_keys if v[n].is_a?(Hash)
-           result.merge! "#{k.to_s}[]" => format_form_data(v[n])
-       end
-      else
-         result[k.to_s] = v.to_s
-      end
-    end
-    result
-  end 
-  alias_method :to_html, :format_form_data
+  require "utility/array"
+  require "utility/hash"
 end
