9ff915728ef90f4b62f362d14a239e6d57faeb2b
zitterf <fabian.zitter@net-up.de>
2015-05-28 14:46:53 +0200

    rewritten create method

--- lib/netup_resource/response_object.rb
+++ lib/netup_resource/response_object.rb
@@ -1,65 +1,56 @@
 module NetupResource  
   class ResponseObject
     require "netup_resource/auto_detect"
-    #create response object
-    def self.create(schema=nil)
-      obj = Class.new do |c|
-        if schema
-          attr_reader :schema
-          define_method(:initialize){instance_variable_set(:@schema,schema)}
-          schema.each{|i| attr_accessor i}
-          define_method(:[]) do |arg|
-            return instance_variable_get(('@'+arg.to_s).to_sym) if schema.include?(arg.to_sym)
-            raise ArgumentError, 'Argument not in Schema'
-          end
-          define_method(:[]=) do |k,v|
-            schema.include?(k.to_sym) ? instance_variable_set(('@'+k.to_s).to_sym,v) : raise(ArgumentError, 'Argument not in Schema')
-          end
-          define_method(:each) do |&proc|
-            return to_enum(:each) unless proc
-            schema.each{|key| proc.call(key,instance_variable_get("@#{key}".to_sym))}
-          end
 
-          define_method(:to_s) do
-            str = "{"
-            schema.each_with_index do |key,i|
-              str += "#{key}: #{instance_variable_get("@#{key}".to_sym)}"
-              str += ", " if i != schema.length - 1
-            end
-            str += "}"
-            return str
-          end
-          
-          define_method(:attributes) do
-            hash = Hash.new
-            schema.each do |attr|
-              hash[attr.to_sym] = instance_variable_get("@#{attr}".to_sym)
-            end
-            return hash
-          end
-          
-          define_method(:try) do |attr_key|
-            answer = nil
-            if schema.include?(attr_key.to_sym)
-              answer = instance_variable_get("@#{attr_key}".to_sym)
-            end
-            return answer
-          end
-          
-          define_method(:errors?) do
-            schema.include?(:error) || schema.include?(:errors)
-          end
-          
-        else
-          attr_accessor :result
-        end
+    #create response object
+    def self.create(init_schema=nil)
+      init_schema ||= [:result]
+
+      Class.new(ResponseObject) do |c|
+        attr_accessor :schema
+        init_schema.each { |i| attr_accessor i }
+
+        define_method(:initialize){instance_variable_set(:@schema,init_schema)}
       end
-      
-      return obj
     end
 
     def self.auto_detect(data)
       AutoDetect.new(data).object
+    end
+
+    def [](arg)
+      return send(arg.to_sym) if schema.include?(arg.to_sym)
+      raise ArgumentError, 'Argument not in Schema'
+    end
+
+    def []=(key, value)
+       schema.include?(key.to_sym) ? try("#{key}=", value) : raise(ArgumentError, 'Argument not in Schema')
+    end
+
+    def each
+      return to_enum(:each) unless block_given?
+
+      schema.each{ |key| yield(key, try(key.to_sym)) }
+    end
+
+    def to_s
+      attributes.inspect
+    end
+
+    def attributes
+      Hash[ schema.map { |attribute_key| [attribute_key, try(attribute_key.to_sym)] } ]
+    end
+
+    def errors?
+       schema.include?(:error) || schema.include?(:errors)
+    end
+
+    def try(attribute_key, *args)
+      if self.respond_to?(attribute_key.to_sym)
+        self.send(attribute_key.to_sym, *args)
+      else
+        nil
+      end
     end
   end
 end
